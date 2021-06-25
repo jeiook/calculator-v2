eec29ee485c176ca6c9a96664c312823f915e503
Jewook Ryu <ryu_jewook@berkeley.edu>
2021-06-25 14:00:27 -0700

    hard-coded html/css, using separate containers

---
+++ main.js
@@ -0,0 +1,24 @@
+function add(x, y) {
+	return x + y;
+}
+
+function subtract(x, y) {
+	return x - y;
+}
+
+function multiply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	return x / y;
+}
+
+function operate(op, x, y) {
+	return op(x, y);
+}
+
+function main() {
+}
+
+main();
