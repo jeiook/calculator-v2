456c7746d82269337ed501094e970aa803b958b0
Jewook Ryu <ryu_jewook@berkeley.edu>
2021-06-26 12:33:21 -0700

    modified html hierarchy to match functional differences

--- main.js
+++ main.js
@@ -18,7 +18,47 @@ function operate(op, x, y) {
 	return op(x, y);
 }
 
+function updateScreen(val, el) {
+	el.textContent = val;
+}
+
 function main() {
+	let screenVal = "";
+	let firstVal = "";
+	let currentOp = null;
+	const ops = {"add": add, "sub": subtract, "mul": multiply, "div": divide};
+	const screen = document.getElementById("screen");
+	const digBtns = document.getElementById("digs-container").childNodes;
+	const opBtns = document.getElementById("ops-container").childNodes;
+	const eqBtn = document.getElementById("equals");
+	const clearBtn = document.getElementById("clear");
+	digBtns.forEach(e => {
+		e.addEventListener("click", () => {
+			screenVal = String(Number(screenVal + e.getAttribute('id')));
+			updateScreen(screenVal, screen);
+		});
+	});
+	opBtns.forEach(e => {
+		e.addEventListener("click", () => {
+			if (!currentOp) {
+				firstVal = screenVal;
+				screenVal = "";
+				updateScreen(screenVal, screen);
+			}
+			currentOp = ops[e.getAttribute('id')];
+		});
+	});
+	eqBtn.addEventListener("click", () => {
+		screenVal = operate(currentOp, Number(firstVal), Number(screenVal));
+		currentOp = null;
+		updateScreen(screenVal, screen);
+	});
+	clearBtn.addEventListener("click", () => {
+		screenVal = "";
+		firstVal = "";
+		currentOp = null;
+		updateScreen(screenVal, screen);
+	});
 }
 
 main();
